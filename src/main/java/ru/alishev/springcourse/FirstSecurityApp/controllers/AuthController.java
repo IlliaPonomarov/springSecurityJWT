0bb139a0fe06093eeabeb905228e64d675666ac9
Illia <chippaxyucac@gmail.com>
2022-07-28 17:07:59 +0200

    JWT auth was implemented

--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/controllers/AuthController.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/controllers/AuthController.java
@@ -1,32 +1,38 @@
 package ru.alishev.springcourse.FirstSecurityApp.controllers;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.alishev.springcourse.FirstSecurityApp.dto.PersonDTO;
 import ru.alishev.springcourse.FirstSecurityApp.models.Person;
+import ru.alishev.springcourse.FirstSecurityApp.security.JWTUtil;
 import ru.alishev.springcourse.FirstSecurityApp.services.RegistrationService;
 import ru.alishev.springcourse.FirstSecurityApp.util.PersonValidator;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 /**
  * @author Neil Alishev
  */
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final RegistrationService registrationService;
     private final PersonValidator personValidator;
+    private JWTUtil jwtUtil;
+    private ModelMapper modelMapper;
 
     @Autowired
-    public AuthController(RegistrationService registrationService, PersonValidator personValidator) {
+    public AuthController(RegistrationService registrationService, PersonValidator personValidator,
+                          ModelMapper modelMapper, JWTUtil jwtUtil) {
         this.registrationService = registrationService;
         this.personValidator = personValidator;
+        this.jwtUtil = jwtUtil;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping("/login")
@@ -40,15 +46,26 @@ public class AuthController {
     }
 
     @PostMapping("/registration")
-    public String performRegistration(@ModelAttribute("person") @Valid Person person,
+    public Map<String, String> performRegistration(@RequestBody @Valid PersonDTO personDTO,
                                       BindingResult bindingResult) {
+
+        Person person = convertToPerson(personDTO);
+
         personValidator.validate(person, bindingResult);
 
-        if (bindingResult.hasErrors())
-            return "/auth/registration";
+        if (bindingResult.hasErrors()) {
+
+
+            return Map.of("message", "Error");
+        }
 
         registrationService.register(person);
+        String token = jwtUtil.generateToken(person.getUsername());
 
-        return "redirect:/auth/login";
+        return Map.of("jwt-token", token);
+    }
+
+    public Person convertToPerson(PersonDTO personDTO){
+        return modelMapper.map(personDTO, Person.class);
     }
 }
--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/dto/AuthenticationDTO.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/dto/AuthenticationDTO.java
@@ -1,27 +1,21 @@
 package ru.alishev.springcourse.FirstSecurityApp.dto;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
-@NoArgsConstructor
-public class PersonDTO {
+public class AuthenticationDTO {
 
     @NotEmpty(message = "Имя не должно быть пустым")
     @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
     private String username;
 
-    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
-    private int yearOfBirth;
-
 
     private String password;
+
+
 }
